b65d122bb0fd5c13b19bcb94265171a6ef6d4520
hopeht909 <hopeht909@gmail.com>
2021-11-09 16:51:13 +0300

    first

---
+++ app/src/main/java/com/example/morepostandgetrequest/UsersData.kt
@@ -0,0 +1,9 @@
+package com.example.morepostandgetrequest
+
+//(3) we create a class from the json link information
+class UsersData : ArrayList<UsersData.UsersDataItem>(){
+    data class UsersDataItem(
+        val location: String,
+        val name: String
+    )
+}
